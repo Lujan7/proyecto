c3981cc90fdf6a0e0ef46e74471389d2b62092ce
Lujan7 <127273416+Lujan7@users.noreply.github.com>
2023-03-08 19:03:37 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("button").onclick = function () {
+    console.log("Capturamos el evento click")
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+   
+}
+   
+
+
+document.getElementById("boton_color").addEventListener("click". function () {
+   documentbodystylebackgroundColor = "#FF0000";
+});
+    
+   
